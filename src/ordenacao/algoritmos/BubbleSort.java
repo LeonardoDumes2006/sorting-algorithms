fdeb10517477c40a97250b743a070db74c8c0704
Leonardo Dumes de Souza <leonardodumesdesouza2006@gmail.com>
2026-07-05 13:12:58 -0300

    Bubble Sort, Insertion Sort e Selection Sort

--- src/ordenacao/algoritmos/BubbleSort.java
+++ src/ordenacao/algoritmos/BubbleSort.java
@@ -1,5 +1,44 @@
 package ordenacao.algoritmos;
 
-public class BubbleSort {
+import ordenacao.util.MetricasOrdenacao;
 
+public class BubbleSort {
+	
+	public static MetricasOrdenacao ordenar(int[] vetor) {
+		MetricasOrdenacao metricas = new MetricasOrdenacao();
+		int n = vetor.length;
+		
+		// Inicia a contagem do tempo antes de começar a ordenar
+		long tempoInicio = System.currentTimeMillis();
+		
+		for(int i = 0; i < n - 1; i++) {
+			boolean houveTroca = false; 
+			
+			for(int j = 0; j < n - 1 - i; j++) {
+				// Aqui ocorre a comparação entre elementos
+				metricas.setComparacoes(metricas.getComparacoes() + 1);
+				
+				if(vetor[j] > vetor[j+1]) {
+					
+					// Aqui ocorre a troca ou movimentação de elementos
+					int temp = vetor[j];
+					vetor[j] = vetor[j+1];
+					vetor[j+1] = temp;
+					
+					metricas.setTrocas(metricas.getTrocas() + 1);
+					houveTroca = true;
+				}
+			}
+			
+			if(!houveTroca) {
+				break;
+			}	
+		}
+		
+		// Finaliza a contagem do tempo e guarda na classe de métricas
+		long tempoFim = System.currentTimeMillis();
+		metricas.setTempoExecucao(tempoFim - tempoInicio);
+		
+		return metricas;
+	}
 }
--- src/ordenacao/algoritmos/InsertionSort.java
+++ src/ordenacao/algoritmos/InsertionSort.java
@@ -1,5 +1,40 @@
 package ordenacao.algoritmos;
 
-public class InsertionSort {
+import ordenacao.util.MetricasOrdenacao;
 
+public class InsertionSort {
+	
+	public static MetricasOrdenacao ordenar(int[] vetor) {
+		MetricasOrdenacao metricas = new MetricasOrdenacao();
+		int n = vetor.length;
+		
+		long tempoInicio = System.currentTimeMillis();
+		
+		for(int i = 1; i < n; i++) {
+			int chave = vetor[i];
+			int j = i - 1;
+			
+			while (j >= 0) {
+				// Aqui ocorre a comparação entre elementos
+				metricas.setComparacoes(metricas.getComparacoes() + 1);
+				
+				if (vetor[j] > chave) {
+					// Aqui ocorre a movimentação (deslocamento) de elementos
+					metricas.setTrocas(metricas.getTrocas() + 1);
+					
+					vetor[j + 1] = vetor[j];
+					j--;
+				} else {
+					// Se o elemento não for maior que a chave, já está no lugar certo
+					break; 
+				}
+			}
+			vetor[j + 1] = chave;
+		}
+		
+		long tempoFim = System.currentTimeMillis();
+		metricas.setTempoExecucao(tempoFim - tempoInicio);
+		
+		return metricas;
+	}
 }
